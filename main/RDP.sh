5e3d6210a3da114731a3596ef8b03c41e60ac29a
ks65678610 <79518138+ks65678610@users.noreply.github.com>
2021-02-23 17:51:20 +0530

    Update RDP.sh

--- main/RDP.sh
+++ main/RDP.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
-printf "hi Done..." >&2
+printf "Check Out My Channel While Waiting- https://youtu.be/VeO22kIvZOQ" >&2
 {
 sudo useradd -m ALOK
 sudo adduser ALOK sudo
 echo 'ALOK:8426' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
-wget https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
-sudo dpkg --install chromeremotedesktophost.msi
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
-wget https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
-sudo dpkg --install chromeremotedesktophost.msi
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo apt -y install obs-studio
